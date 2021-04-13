e7cb99f56ec25db0420d841be4c6797beedebbac
ZachGrande <zach.grande23@gmail.com>
2021-04-13 09:23:44 -0700

    Testing updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const viewWidth = 500;
 const App = () => {
     return <svg style={{border: "1px solid lightgrey", width: viewWidth, height: viewHeight}}>
         <circle cx={0} cy={viewHeight} r="5" />
+        <p>This is working</p>
         <rect x="200" y="100" width="10" height="10" />
         <rect x={200} y={200} width={10} height={10} fill="rgb(230,230,230)" />
         <rect x={212} y={200} width={10} height={10} fill="rgb(230,230,230)" />
