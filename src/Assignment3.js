bd2c6c58f4bca841e7e0ccd270f5d89361e1d872
dma150 <dma150@uw.edu>
2021-05-12 10:48:59 -0700

    added usa json file

--- src/Assignment3.js
+++ src/Assignment3.js
@@ -3,13 +3,14 @@ import { useFetch } from "./hooks/useFetch";
 import { useJson } from "./hooks/useJson";
 import { geoPath, geoAlbersUsa } from 'd3-geo';
 import * as topojson from "topojson-client";
-import world from "./land-50m";
+import world from "./cb_2015_us_state_20m.json";
 import { extent } from "d3-array";
 import { scaleLinear } from "d3-scale";
 import {
     ComposableMap,
     Geographies,
     Geography,
+    ZoomableGroup
 } from "react-simple-maps"
 
 /*
@@ -24,21 +25,23 @@ function Assignment2() {
     );
     
     return (
-        
         <div>
-        <h1>Assignment 3: Interactive Data Visualization</h1>
-        <h3>Zach Grande, Alycia Nguyen, Michelle Ponting, Darren Ma, Erik Thomas-Hommer</h3>
+            <h1>Assignment 3: Interactive Data Visualization</h1>
+            <h3>Zach Grande, Alycia Nguyen, Michelle Ponting, Darren Ma, Erik Thomas-Hommer</h3>
         <p>{loading && "Loading data!"}</p>
         <ComposableMap>
-        <Geographies geography={world}>
-        {({geographies}) => geographies.map(geo =>
-            <Geography key={geo.rsmKey} geography={geo} />
+            <ZoomableGroup zoom={1}>
+                <Geographies geography={world}>
+                    {({geographies}) => 
+                      geographies.map(geo =>
+                        <Geography key={geo.rsmKey} geography={geo} />
             )}
-            </Geographies>
-            </ComposableMap>
+                </Geographies>
+            </ZoomableGroup>
+        </ComposableMap>
             
-            </div>
-            )
+    </div>
+    )
         }
         
         export default Assignment2;
