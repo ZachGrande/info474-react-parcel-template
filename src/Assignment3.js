3bd950ad7681b99086aaac4eb4b5f5b8a6d86fb1
ZachGrande <zach.grande23@gmail.com>
2021-05-13 21:19:54 -0700

    Coordinates render, map does not

--- src/Assignment3.js
+++ src/Assignment3.js
@@ -8,11 +8,6 @@ import * as us from "./states-albers-10m.json";
 import world from "./land-50m";
 import { extent } from "d3-array";
 import { scaleLinear } from "d3-scale";
-// import {
-//     ComposableMap,
-//     Geographies,
-//     Geography,
-// } from "react-simple-maps"
 
 /*
 * The skeleton for the map was implemented using the documentation for D3's Bubble Map
@@ -28,30 +23,24 @@ function Assignment3() {
     const chart = function() {
 
         const svg = d3.select("#map");
-        const path = d3.geoPath()
-        // const projection = d3.geoAlbersUsa().scale(1200).translate([960/2, 600/2]);
-        const radius = d3.scaleSqrt([0, d3.max(data, d => d.total_volume)], [0, 40]) // altered line, was d.value
-        // const translate = (coords) => {
-        //     const [x, y]  = projection([+coords.longitude, +coords.latitude])
-        //     return `translate(${x},${y})`
-        // };
 
-        // svg.append("g")
-        //     .attr("fill", "brown")
-        //     .attr("fill-opacity", 0.5)
-        //     .attr("stroke", "#fff")
-        //     .attr("stroke-width", 0.5)
-        //     .selectAll("circles")
-        //     .data(data)
-        //     .join("circle")
-        //     .attr("transform", translate)
-        //     .attr("r", d => radius(d.count));
+        // This path variable works when building the map
+        // const path = d3.geoPath();
 
+        // This construction of projection/path can translate the coordinates, but not render the map
+        const projection = d3.geoNaturalEarth1();
+        const path = d3.geoPath(projection);
+
+
+        const radius = d3.scaleSqrt([0, d3.max(data, d => d.total_volume)], [0, 40]); // altered line, was d.value
+
+        // Draw US outline
         svg.append("path")
             .datum(topojson.feature(us, us.objects.nation))
             .attr("fill", "#ddd")
             .attr("d", path);
 
+        // Draw states outline
         svg.append("path")
             .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
             .attr("fill", "none")
@@ -59,6 +48,7 @@ function Assignment3() {
             .attr("stroke-linejoin", "round")
             .attr("d", path);
 
+        // Create legend
         const legend = svg.append("g")
             .attr("fill", "#777")
             .attr("transform", "translate(915,608)")
@@ -68,17 +58,20 @@ function Assignment3() {
             .data(radius.ticks(4).slice(1))
             .join("g");
 
+        // Add cirlces to legend
         legend.append("circle")
             .attr("fill", "none")
             .attr("stroke", "#ccc")
             .attr("cy", d => -radius(d))
             .attr("r", radius);
 
+        // Add text to legend
         legend.append("text")
             .attr("y", d => -2 * radius(d))
             .attr("dy", "1.3em")
             .text(radius.tickFormat(4, "s"));
 
+        // Draw circles on map
         svg.append("g")
             .attr("fill", "brown")
             .attr("fill-opacity", 0.5)
@@ -86,11 +79,13 @@ function Assignment3() {
             .attr("stroke-width", 0.5)
             .selectAll("circle")
             .data(data
-                .filter(d => d.city)
+                .filter(d => d.latitude)
                 // .sort((a, b) => d3.descending(a.latitude, b.longitude)))
-                .sort((a, b) => d3.descending(a.latitude, b.latitude))) // altered line, was a/b.value
+                .sort((a, b) => d3.descending(a.total_volume, b.total_volume))) // altered line, was a/b.value
                 .join("circle")
-                .attr("transform", d => `translate(${d.latitude})`) // altered line, was d.position
+                // .attr("transform", d => `translate(${+d.latitude},${+d.longitude})`) // altered line, was d.position
+                .attr("transform", d=>`translate(
+                    ${projection([d.longitude, d.latitude])})`)
                 .attr("r", d => radius(d.total_volume)) // altered line: .attr("r", d => radius(d.value))
                 .append("title")
                 // .text(d => `${d.city}
