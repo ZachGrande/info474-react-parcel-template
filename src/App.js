1d1b93a0f853d4c803511e9074af94e8095603a9
ZachGrande <zach.grande23@gmail.com>
2021-05-24 19:32:24 -0700

    Added tab for final project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './HomePage.js';
 import Assignment2 from './Assignment2';
 import InteractiveDemos from './InteractiveDemos';
 import Assignment3 from './Assignment3';
+import Assignment4 from './Assignment4';
 import { Route, Switch, Link, Redirect, NavLink } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -26,11 +27,13 @@ function App(props) {
                     </div>
                     <div className="col-9">
                         <Switch>
-                            <Route exact path="/info474-react-parcel-template/" component={HomePage} />
+                            <Route exact path="/info474-react-parcel-template/" component={Assignment4} />
+                            <Route path="/info474-react-parcel-template/home" component={HomePage} />
                             <Route path="/info474-react-parcel-template/demos" component={ClassDemos} />
                             <Route path="/info474-react-parcel-template/assignment2" component={Assignment2} />
                             <Route path="/info474-react-parcel-template/demos2" component={InteractiveDemos} />
                             <Route path="/info474-react-parcel-template/assignment3" component={Assignment3} />
+                            
                             <Redirect to="/info474-react-parcel-template/" />
                         </Switch>
                     </div>
@@ -46,7 +49,8 @@ function App(props) {
 function AboutNav() {
     return (
         <Navbar bg="navbar navbar-expand-lg navbar-light bg-light" id="nav-bar">
-            <NavLink exact to="/info474-react-parcel-template/" className="nav-link" id="nav-li" activeClassName={"activeLink"}>Home Page</NavLink>
+            <NavLink exact to="/info474-react-parcel-template/" className="nav-link" id="nav-li" activeClassName={"activeLink"}>Final Project</NavLink>
+            <NavLink exact to="/info474-react-parcel-template/home" className="nav-link" id="nav-li" activeClassName={"activeLink"}>Home Page</NavLink>
             <NavLink exact to="/info474-react-parcel-template/demos" className="nav-link" id="nav-li" activeClassName={"activeLink"}>Class Demos</NavLink>
             <NavLink exact to="/info474-react-parcel-template/assignment2" className="nav-link" id="nav-li" activeClassName={"activeLink"}>Assignment 2</NavLink>
             <NavLink exact to="/info474-react-parcel-template/demos2" className="nav-link" id="nav-li" activeClassName={"activeLink"}>Interactive Demos</NavLink>
