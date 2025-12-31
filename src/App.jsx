7f36e0ba711e8927c978cf2601b718f767c15ea0
Zach Grande <zach.grande23@gmail.com>
2025-12-31 11:56:57 -0800

    fix: update deps, migrate bootstrap 4 to 5

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
     return (
         <div>
-            <header className="jumbotron jumbotron-fluid py-4 mb-0">
+            <header className="bg-light py-4 mb-0">
                 <div className="d-flex mx-5 px-5 flex-column">
                     <div>
                         <h1><Link to="/"> INFO 474: Data Visualization </Link></h1>
@@ -38,7 +38,7 @@ function App() {
                 </Routes>
             </main>
             <footer className="container pt-5">
-                <p className="float-right">University of Washington</p>
+                <p className="float-end">University of Washington</p>
             </footer>
         </div>
     );
@@ -48,12 +48,12 @@ function AboutNav() {
     const basePath = '/info474-react-parcel-template'
     return (
         <Navbar bg="navbar navbar-expand-lg navbar-light bg-transparent" id="nav-bar">
-            <NavLink to="/" className="nav-link" id="nav-li" activeclassname={"activeLink"}>Final Project</NavLink>
-            <NavLink to="/home" className="nav-link" id="nav-li" activeclassname={"activeLink"}>Home Page</NavLink>
-            <NavLink to="/demos" className="nav-link" id="nav-li" activeclassname={"activeLink"}>Class Demos</NavLink>
-            <NavLink to="/assignment2" className="nav-link" id="nav-li" activeclassname={"activeLink"}>Assignment 2</NavLink>
-            <NavLink to="/demos2" className="nav-link" id="nav-li" activeclassname={"activeLink"}>Interactive Demos</NavLink>
-            <NavLink to="/assignment3" className="nav-link" id="nav-li" activeclassname={"activeLink"}>Assignment 3</NavLink>
+            <NavLink to="/" className="nav-link link-primary me-3" id="nav-li" activeclassname={"activeLink"}>Final Project</NavLink>
+            <NavLink to="/home" className="nav-link link-primary me-3" id="nav-li" activeclassname={"activeLink"}>Home Page</NavLink>
+            <NavLink to="/demos" className="nav-link link-primary me-3" id="nav-li" activeclassname={"activeLink"}>Class Demos</NavLink>
+            <NavLink to="/assignment2" className="nav-link link-primary me-3" id="nav-li" activeclassname={"activeLink"}>Assignment 2</NavLink>
+            <NavLink to="/demos2" className="nav-link link-primary me-3" id="nav-li" activeclassname={"activeLink"}>Interactive Demos</NavLink>
+            <NavLink to="/assignment3" className="nav-link link-primary" id="nav-li" activeclassname={"activeLink"}>Assignment 3</NavLink>
         </Navbar>
     )
 }
